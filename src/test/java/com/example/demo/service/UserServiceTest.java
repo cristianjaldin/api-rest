f84f86df206ee41686d22bbfe6262629103a8713
Cristian Jaldin <cristianjaldin@hotmail.com>
2020-08-02 17:16:58 -0300

    TEST

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 
-
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -12,39 +11,43 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 
 import com.example.demo.entity.User;
 import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.repository.UserRepository;
 
-
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class UserServiceTest {
-	
+
+    private final static String USER_ID = "utybfjslfjghywtqbviffnejsapjdw";
+    private final static String NONEXISTENT_USER_ID = "dfgfgfdgdfgdfgdfgdretertertret";
+    
 	@MockBean
 	private UserRepository mockUserRepository;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Test
-    void testFindById() throws UserNotFoundException {
-		
-		doReturn(Optional.of(getUser())).when(mockUserRepository).findById("utybfjslfjghywtqbviffnejsapjdw");
-		User user = userService.findById("utybfjslfjghywtqbviffnejsapjdw");
-	    assertEquals("utybfjslfjghywtqbviffnejsapjdw", user.getId());
-    }
-	
+	void testFindById() throws UserNotFoundException {
+		doReturn(Optional.of(getUser())).when(mockUserRepository).findById(USER_ID);
+		User user = userService.findById(USER_ID);
+		assertEquals(USER_ID, user.getId());
+	}
+
 	@Test
-    void testFindByIdNotFound() {
-		
-    }
-	
+	void testFindByIdNotFound() {
+		assertThrows(UserNotFoundException.class, () -> {
+			userService.findById(NONEXISTENT_USER_ID);
+		});
+	}
+
 	public User getUser() {
 		User oneUser = new User();
-		oneUser.setId("utybfjslfjghywtqbviffnejsapjdw");
+		oneUser.setId(USER_ID);
 		oneUser.setName("Cristian");
 		oneUser.setSurname("Jaldin");
 		oneUser.setUsername("cjaldin");
